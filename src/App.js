b3be8b2908f71612fdc0cc5ca9e09b561487c920
unknown <Administrator@IT-CIEMIEGAK.brwsa.com.pl>
2022-03-17 14:14:38 +0100

    changes

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import app from "./style/app.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="container-1">
+        <div className="container-1-box">
+          <div className="container-1-box-image"></div>
+          <p className="container-1-box-first">Daniel Clifford</p>
+          <p className="container-1-box-second">Verified Graduate</p>
+        </div>
+        <p className="container-1-text1">
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking.Lorem Ipsum is that it has a
+          more-or-less normal distribution of letters.
+        </p>
+        <p className="container-1-text2">
+          There are many variations of passages of Lorem Ipsum available, but
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which don't look even slightly believable.
+          If you are going to use a passage of Lorem Ipsum, you need to be sure
+          there isn't anything embarrassing hidden in the middle of text. All
+          the Lorem Ipsum generators on the Internet tend to repeat predefined
+          chunks as necessary.
+        </p>
+      </div>
+      <div className="container-2">
+        <div className="container-2-box">
+          <div className="container-2-box-image"></div>
+          <p className="container-2-box-first">Jonathan Walters</p>
+          <p className="container-2-box-second">Verified Graduate</p>
+        </div>
+        <div className="container-2-text1">
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It
+          has roots in a piece
+        </div>
+        <div className="container-2-text2">
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal sometimes by
+          accident, sometimes on purpose
+        </div>
+      </div>
+      <div className="container-3">
+        <div className="container-3-box">
+          <div className="container-3-box-image"></div>
+          <p className="container-3-box-first">Kira Whittle</p>
+          <p className="container-3-box-second">Verified Graduate</p>
+        </div>
+      </div>
+      <div className="container-4"></div>
+      <div className="container-5"></div>
+    </div>
+  );
+}
+
+export default App;
