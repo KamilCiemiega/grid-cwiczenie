d931868fe26b2b234930c0afb7f253023d98b61c
unknown <Administrator@IT-CIEMIEGAK.brwsa.com.pl>
2022-03-18 08:23:26 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -47,9 +47,57 @@ function App() {
           <p className="container-3-box-first">Kira Whittle</p>
           <p className="container-3-box-second">Verified Graduate</p>
         </div>
+        <div className="container-3-text1">
+          There are many variations of passages of Lorem Ipsum available, but
+          the majority have suffered alteration in some form.
+        </div>
+        <div className="container-3-text2">
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It
+          has roots in a piece of classical Latin literature from 45 BC, making
+          it over 2000 years old. Richard McClintock, a Latin professor at
+          Hampden-Sydney College in Virginia, looked up one of the more obscure
+          Latin words, consectetur, from a Lorem Ipsum passage, and going
+          through the cites.Richard McClintock, a Latin professor at
+          Hampden-Sydney College in Virginia, looked up one of the more obscure
+          Latin words, consectetur, from a Lorem Ipsum passage, and going
+          through the cites.from a Lorem Ipsum passage, and going through the
+          cites.
+        </div>
       </div>
-      <div className="container-4"></div>
-      <div className="container-5"></div>
+      <div className="container-4">
+        <div className="container-4-box">
+          <div className="container-4-box-image"></div>
+          <p className="container-4-box-first">Jenette Harmon</p>
+          <p className="container-4-box-second">Verified Graduate</p>
+        </div>
+        <div className="container-4-text1">
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It
+          has roots in a piece
+        </div>
+        <div className="container-4-text2">
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal sometimes by
+          accident, sometimes on purpose
+        </div>
+      </div>
+      <div className="container-5">
+        <div className="container-5-box">
+          <div className="container-5-box-image"></div>
+          <p className="container-5-box-first">Patrick Harmon</p>
+          <p className="container-5-box-second">Verified Graduate</p>
+        </div>
+        <div className="container-5-text1">
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It
+          has roots in a piecenot simply random text. It has roots in a piece
+        </div>
+        <div className="container-5-text2">
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal sometimes by
+          accident, sometimes on purpose
+        </div>
+      </div>
     </div>
   );
 }
